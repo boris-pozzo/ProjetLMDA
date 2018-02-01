b7789086018b88468f5a3c8af8f0347562cd90a1
Boris <boris.pozzo@gmail.com>
2018-02-01 16:36:27 -0400

    test header

--- src/Header.js
+++ src/Header.js
@@ -7,21 +7,21 @@ class Header extends Component {
     render() {
         return (
             <div className="Header">
-           
-            <div className="divLMDA">
-            <NavLink to="/Accueil"><img src={LMDA} alt="logo" className="LMDA"/></NavLink>
+
+                <div className="divLMDA">
+                    <NavLink to="/Accueil"><img src={LMDA} alt="logo" className="LMDA" /></NavLink>
+                </div>
+                <div className="divLiens">
+                    <NavLink to="/Accueil" className="Liens">HOME</NavLink>
+                    <NavLink to="/Presentation" className="Liens">PRESENTATION</NavLink>
+                    <NavLink to="/Produits" className="Liens">PRODUITS</NavLink>
+                    <NavLink to="/Createurs" className="Liens">CREATEURS</NavLink>
+                    <NavLink to="/Contact" className="Liens">CONTACT</NavLink>
+
+
+                </div>
             </div>
-            <div className="divLiens">
-                <NavLink to="/Accueil" className="Liens">ACCUEIL</NavLink>
-                <NavLink to="/Presentation" className="Liens">PRESENTATION</NavLink>
-                <NavLink to="/Produits" className="Liens">PRODUITS</NavLink>
-                <NavLink to="/Createurs" className="Liens">CREATEURS</NavLink>
-                <NavLink to="/Contact" className="Liens">CONTACT</NavLink>
-              
-                
-          </div>
-            </div>
-        
+
         )
     }
 };
